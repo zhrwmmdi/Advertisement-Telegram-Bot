dfad713f72ca2bd51817e677fa40e6bc4d05f6eb
zhrwmmdi <zahra.mmdi2003@gmail.com>
2024-09-09 13:49:16 +0330

    database methods updated

--- mongo_client.py
+++ mongo_client.py
@@ -2,37 +2,75 @@ import pymongo
 
 
 class AdsMongoClient:
-    def __init__(self, host: str, port: int, db_name: str = "telegram_bot", ads_collection_name: str = "ads", categories_collection_name: str = "categories",):
+    def __init__(
+            self,
+            host: str,
+            port: int,
+            db_name: str = "telegram_bot",
+            ads_collection_name: str = "ads",
+            categories_collection_name: str = "categories",
+    ):
         self.client = pymongo.MongoClient(host, port)
         self.db = self.client.get_database(db_name)
         self.ads_collection = self.db.get_collection(ads_collection_name)
         self.categories_collection = self.db.get_collection(categories_collection_name)
 
     def add_category(self, category: str):
-        # write your code here
-        pass
+        self.categories_collection.insert_one(
+            {
+                "category": category,
+            }
+        )
 
     def get_categories(self) -> list:
-        # write your code here
-        pass
+        results = self.categories_collection.find()
+        return [result["category"] for result in results]
 
     def add_advertising(
-        self, user_id: int, photo_url: str, category: str, description: str
+            self, user_id: int, photo_url: str, category: str, description: str
     ):
-        # write your code here
-        pass
+        self.ads_collection.insert_one(
+            {
+                "user_id": user_id,
+                "description": description,
+                "category": category,
+                "photo_url": photo_url,
+            }
+        )
 
     def delete_advertising(self, user_id: int, doc_id: str):
-        # write your code here
-        pass
+        self.ads_collection.delete_one(
+            {
+                "_id": doc_id,
+                "user_id": user_id,
+            }
+        )
 
     def get_ads_by_user_id(self, user_id: int):
-        # write your code here
-        pass
+        results = self.ads_collection.find({"user_id": user_id})
+        return [
+            {
+                "id": str(result["_id"]),
+                "photo_url": result["photo_url"],
+                "category": result["category"],
+                "description": result["description"],
+            }
+            for result in results
+        ]
 
     def get_ads_by_category(self, category: str):
-        # write your code here
-        pass
+        results = self.ads_collection.find(
+            {"category": {"$regex": ".*" + category + ".*"}}
+        )
+        return [
+            {
+                "id": str(result["_id"]),
+                "photo_url": result["photo_url"],
+                "category": result["category"],
+                "description": result["description"],
+            }
+            for result in results
+        ]
 
 
 if __name__ == "__main__":
